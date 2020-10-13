1dea629d1aa66d2ba92c4d4afa3df7de8b2f6ba9
Jan Bernhard <janhenrikbern@users.noreply.github.com>
2020-10-13 12:12:02 -0400

    formating

--- data_tools/featurizer.py
+++ data_tools/featurizer.py
@@ -2,20 +2,27 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def get_page_count(first_page, last_page):
-    return [int(l) - int(f) if l and f is not None else None for f, l in zip(first_page, last_page)]
+    return [
+        int(l) - int(f) if l and f is not None else None
+        for f, l in zip(first_page, last_page)
+    ]
 
 
 le = {}
 from sklearn.preprocessing import LabelEncoder
+
+
 def encode_categorical(df, cols):
     t = df.copy().dropna(subset=cols)
     global le
     for col in cols:
         le[col] = LabelEncoder()
         t[col] = le[col].fit_transform(t[col])
-    
+
     return t
+
 
 def decode_categorical(df, cols):
     """
@@ -25,9 +32,6 @@ def decode_categorical(df, cols):
     global le
     for col in cols:
         if col in le.keys():
-            t[col] = le[col].inverse_transform(t.loc[:,col])
+            t[col] = le[col].inverse_transform(t.loc[:, col])
 
     return t
-
-
- 
