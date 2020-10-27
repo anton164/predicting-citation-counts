fdd13fe18ee62564da8758feef083e592002bd3d
Jan Bernhard <janhenrikbern@users.noreply.github.com>
2020-10-27 18:26:27 -0400

    some minor formatting

--- experiments/base.py
+++ experiments/base.py
@@ -2,14 +2,13 @@ import numpy as np
 import pickle
 from sklearn.model_selection import (
     train_test_split,
-
 )
 
-class Experiment():
 
+class Experiment:
     def __init__(self, pandas_df):
-        self.X = pandas_df.iloc[:,:-1]
-        self.y = pandas_df.iloc[:,-1]
+        self.X = pandas_df.iloc[:, :-1]
+        self.y = pandas_df.iloc[:, -1]
         self.model = None
 
     def run(self):
@@ -20,23 +19,25 @@ class Experiment():
         pass
 
     def split(self, split=0.8):
-        self.X_train, self.X_test, self.y_train, self.y_test =  train_test_split(self.X, self.y, test_size=split, random_state=42)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            self.X, self.y, test_size=split, random_state=42
+        )
 
     def preprocess(self):
-        pass 
+        pass
 
     def train(self):
-        pass 
+        pass
 
     def evaluate(self):
         pass
 
     def save(self, filename):
         if self.model is not None:
-            pickle.dump(self.model, open(filename, 'wb'))
+            pickle.dump(self.model, open(filename, "wb"))
 
     def load(self, filename):
         if self.model is None:
-            self.model = pickle.load(open(filename, 'rb'))
+            self.model = pickle.load(open(filename, "rb"))
         else:
             return "Error: Can't overwrite existing experiment"
