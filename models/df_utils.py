1e987ad0067c206db6076ef2ed324d3ea24d01e6
Anton Abilov <anton.abilov@gmail.com>
2020-12-12 20:59:36 +0100

    Updated df_utils to simplify results df

--- models/df_utils.py
+++ models/df_utils.py
@@ -7,26 +7,25 @@ def load_df(filename):
 def join_dfs(df1, df2):
     return df1.join(df2, on='PaperId')
 
-def create_error_analysis_df(y_paper_ids, y_true, y_pred, full_df):
+def create_results_df(y_paper_ids, y_true, y_pred):
     """ 
-        Creates an error analysis df with cols from the full df and: 
+        Create a results df to save model predictions: 
         TrueClass, PredictedClass, Misclassified,
         MisclassifiedAsHigh, MisclassifiedAsLow 
     """
-    error_analysis_df = pd.DataFrame(
-        list(zip(y_index, y_true, y_pred)),
+    results_df = pd.DataFrame(
+        list(zip(y_paper_ids, y_true, y_pred)),
         columns=["PaperId", "TrueClass", "PredictedClass"]
     ).set_index("PaperId")
 
-    error_analysis_df = join_dfs(error_analysis_df, full_df)
-    error_analysis_df["Misclassified"] = error_analysis_df.apply(lambda x: 1 if x["TrueClass"] != x["PredictedClass"] else 0, axis=1)
-    error_analysis_df["MisclassifiedAsHigh"] = error_analysis_df.apply(
+    results_df["Misclassified"] = results_df.apply(lambda x: 1 if x["TrueClass"] != x["PredictedClass"] else 0, axis=1)
+    results_df["MisclassifiedAsHigh"] = results_df.apply(
         lambda x: 1 if x["PredictedClass"] == 1 and x["Misclassified"] else 0, 
         axis=1
     )
-    error_analysis_df["MisclassifiedAsLow"] = error_analysis_df.apply(
+    results_df["MisclassifiedAsLow"] = results_df.apply(
         lambda x: 1 if x["PredictedClass"] == 0 and x["Misclassified"] else 0, 
         axis=1
     )
 
-    return error_analysis_df
+    return results_df
